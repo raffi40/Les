2c75b1defb3e27a8c32ef10e4590f8db7aa46211
raffi40 <raffi_halajian@yahoo.com>
2017-05-10 13:41:50 +0400

    Add files via upload

---
+++ my9-1/js/jav9.js
@@ -0,0 +1,69 @@
+
+
+var colors = ['Pink', 'Blue', 'White','Red', 'Black'];
+console.log (colors);
+
+
+
+var colors = prompt ("Which color is your favorite? Pink, Blue, White, Red, Black");
+switch (colors){
+case ("Pink"):
+document.write ("My life is pink </br>");
+break;
+
+case ("Blue"):
+document.write ("Clear sky </br>");
+break;
+
+case ("White"):
+document.write ("I have a white heart </br>");
+break;
+
+case ("Red"):
+document.write ("Shining day forever </br>");
+break;
+
+case ("Black"):
+document.write ("Elegance </br>");
+break;
+
+default:
+document.write ("Black heart </br>");
+}
+
+
+
+
+
+
+var col = prompt ("Choose a number among 1,2,3,4,5 to know your color");
+
+parse int 
+if (col = 1){
+document.write ("Pink");
+}
+else if (col = 2){
+document.write ("Blue");
+}
+else if (col = 3){
+document.write ("White");
+}
+else if (col = 4){
+document.write ("Red");
+}
+else if (col = 5){
+document.write ("Black");
+}
+else {
+document.write (" Do You Mean A Rainbow ???");
+};
+
+
+
+
+
+
+
+
+
+
